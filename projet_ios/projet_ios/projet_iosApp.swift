958912f7f66dccde4781d286af615824357331ec
td4555 <122525654+DOLINGO@users.noreply.github.com>
2025-12-18 12:32:32 +0100

    initialisation projet
    Co-Authored-By: Setif94 <145097589+Setif94@users.noreply.github.com>

---
+++ projet_ios/projet_ios/projet_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  projet_iosApp.swift
+//  projet_ios
+//
+//  Created by imac12 on 17/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct projet_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
